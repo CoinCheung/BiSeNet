a2fb1dad39539935ce265884610328969fdc2e6d
Saumya Kumaar <kumaar324@gmail.com>
2020-05-19 10:54:47 +0200

    Visualization and FPS

--- demo.py
+++ demo.py
@@ -5,11 +5,11 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from PIL import Image
 import cv2
-
-from fp16.model import BiSeNet
-
-
-# args
+import datetime
+from model_061_new import DABLNet
+import statistics as stat
+from visualize import get_color_pallete
+import os
 parse = argparse.ArgumentParser()
 parse.add_argument(
         '--ckpt',
@@ -25,7 +25,7 @@ args = parse.parse_args()
 
 
 # define model
-net = BiSeNet(n_classes=19)
+net = DABLNet(n_classes=19)
 net.load_state_dict(torch.load(args.ckpt, map_location='cpu'))
 net.eval()
 net.cuda()
@@ -37,6 +37,17 @@ to_tensor = transforms.Compose([
     ])
 im = to_tensor(Image.open(args.img_path).convert('RGB')).unsqueeze(0).cuda()
 
+times = []
 # inference
-out = net(im)[0].argmax(dim=1).squeeze().detach().cpu().numpy()
-cv2.imwrite('./res.jpg', out)
+for i in range(10):
+    start_time = datetime.datetime.now()
+    out = net(im)[0].argmax(dim=1).squeeze().detach().cpu().numpy()
+    end_time = datetime.datetime.now()
+    delta = end_time - start_time
+    print('Prediction {} took {} ms'.format(i+1, int(delta.total_seconds() * 1000)))
+    times.append(int(delta.total_seconds() * 1000))
+
+mask = get_color_pallete(out, 'citys')
+mask.save('res.png')
+print('Average Time: {} ms'.format(stat.mean(times)))
+print('Average FPS: {}'.format(1000/stat.mean(times)))
