2c8cc478aca11bf15225d8922d51e46022526474
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2025-04-07 04:25:28 +0000

    Add further improvements to coreml conversion logic

--- tools/convert_to_coreml.py
+++ tools/convert_to_coreml.py
@@ -22,7 +22,7 @@ parse.add_argument('--config', dest='config', type=str, default='configs/bisenet
 parse.add_argument('--weight-path', type=str, default='./lib/models/model_zoo/model_final_v2_city.pth',)
 parse.add_argument('--fp16', action='store_true')
 parse.add_argument('--outpath', dest='out_pth', type=str,
-        default='model.mlmodel')
+        default='model.mlpackage')
 parse.add_argument('--img-path', dest='img_path', type=str, default='./datasets/custom_images/test.jpg',)
 args = parse.parse_args()
 
@@ -40,24 +40,47 @@ to_tensor = T.ToTensor(
 )
 print('Loading image:', args.img_path)
 im = cv2.imread(args.img_path)[:, :, ::-1]
+# Resize
+im = cv2.resize(im, (512, 256))
 im = to_tensor(dict(im=im, lb=None))['im'].unsqueeze(0)#.cuda()
 
 class WrappedBiSeNetv2(nn.Module):
     def __init__(self, cfg):
         super(WrappedBiSeNetv2, self).__init__()
-        self.model = model_factory[cfg.model_type](cfg.n_cats, aux_mode='pred')
+        self.model = model_factory[cfg.model_type](cfg.n_cats, aux_mode='eval')
+        self.n_cats = cfg.n_cats
         self.model.load_state_dict(torch.load(args.weight_path), strict=False)
         self.model.eval()
 
     def forward(self, x):
-        return self.model(x)[0]
+        res = self.model(x)[0]
+        out = torch.argmax(res, dim=1, keepdim=True)
+        out = out.float() / 255
+        return out
 
 torch_model = WrappedBiSeNetv2(cfg).eval()
 traced_model = torch.jit.trace(torch_model, im)
 
 mlmodel_from_trace = ct.convert(
     traced_model,
-    inputs=[ct.TensorType(name="input", shape=im.shape)],
+    inputs=[ct.ImageType(name="input", shape=im.shape)],
+    outputs=[ct.ImageType(name="output", color_layout=ct.colorlayout.GRAYSCALE)],
+    # compute_precision=ct.precision.FLOAT16
+    # minimum_deployment_target=ct.target.iOS13,
+    # compute_units=ct.ComputeUnit.CPU_AND_GPU
+
 )
 mlmodel_from_trace.save(args.out_pth)
 print(f"Saved the model to {args.out_pth}")
+
+
+# Model list
+# model1: model with multiarray input and output, image size (2048 x 922)
+# model2: model with multiarray input and output, image size (1024 x 512)
+# model3: model with image input and multiarray output, image size (1024 x 512)
+# model4: model with image input and multiarray output, image size (1024 x 512)
+# model5: model with image input and image output, image size (1024 x 512)
+# model6: model with image input and image output, image size (1024 x 512)
+# model7: model with image input and image output, image size (1024 x 512), cpu and gpu
+# model8: model with image input and image output, image size (1024 x 512), with fp16 (which happens to be default anyway)
+# model9: model with image input and image output, image size (512 x 256)
